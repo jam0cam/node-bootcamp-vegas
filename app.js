e7e8b65fb903b9eae706a4603adaa7bb9799f10c
Jia <jitse@zappos.com>
2013-06-15 14:21:15 -0700

    using connection.remoteAddress instead of handshake

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ io.on('connection', function(socket) {
 
   //this is called when a answer is submitted in the format {number, response}
   socket.on('answer', function(data){
-    var address = socket.handshake.address.address;
+    //var address = socket.handshake.address.address;
+    var address = socket.connection.remoteAddress.address;
 
     if (data.number == '1' && winners[0] == '0'){      
       if (data.response == '2') {
